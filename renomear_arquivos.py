cdd734b89e46ba8492fc63410f0e242d149fba6c
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2023-08-05 01:18:22 -0300

    Update renomear_arquivos.py

--- renomear_arquivos.py
+++ renomear_arquivos.py
@@ -1,12 +1,9 @@
 import os
 contador = 1
-# Obtenha o endereço de memória do usuário
 endereco = input("Digite o endereço de memória: ")
 
-# Etiqueta sera o nome do arquivo, tipo "Video1.mp4", "Video2.mp4"
 nome_usuario = input("Digite a nova etiqueta: ")
 
-# Obtem  lista de todos os arquivos no diretório especificado  
 arquivos = os.listdir(endereco)
 
 for arquivo in arquivos:
