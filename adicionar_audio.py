16b8f87c67951107814aeea3cc61245d7e3b656b
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2023-08-05 01:18:42 -0300

    Update adicionar_audio.py

--- adicionar_audio.py
+++ adicionar_audio.py
@@ -1,16 +1,11 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-# recebe o endereço do vídeo
 endereco_video = input("Digite o endereço do vídeo: ")
 
-# cria um objeto do tipo VideoFileClip
 clip = VideoFileClip(endereco_video)
 
-# extrai o áudio do vídeo
 audio = clip.audio
 
-# salva o áudio em um arquivo .mp3
 audio.write_audiofile("audio.mp3")
 
-# fecha o objeto VideoFileClip
 clip.close()
